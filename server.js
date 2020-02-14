a6eba59f3f9f68b6550f66e09e15540d9363980e
John-Oula <johnoula@icloud.com>
2020-02-14 19:30:16 +0800

    first

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const parser = require("body-parser");
+const app = express();
+
+app.use(parser.urlencoded({extended : true }));
+
+function alert(){
+	$.alert("discover");
+}
+app.get("/",function(req,res){
+
+	res.sendFile(__dirname + "/index.html");
+}
+);
+
+app.post("/",function(req,res){
+	var num1 = Number(req.body.num1);
+	var result = num1 * 10;
+	res.send("eqeddq" + result);
+}
+);
+
+app.listen(3000,function(){
+	console.log("started");
+});
